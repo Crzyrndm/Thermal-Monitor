b5727c8e92abf6b3444b1474b65f569a5f788310
Crzyrndm <joshcrawfy@gmail.com>
2015-06-03 17:21:17 +1200

    fixed engines heat generation irregularly ignored

--- Thermal Monitor/Thermal Monitor/Thermal Monitor.cs
+++ Thermal Monitor/Thermal Monitor/Thermal Monitor.cs
@@ -10,7 +10,7 @@ namespace Thermal_Monitor
     {
         [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Temperature", guiUnits = " K", guiFormat = "F2")]
         public float temp;
-        [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Degrees / s", guiUnits = " K", guiFormat = "F2")]
+        [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Degrees / s", guiUnits = " K", guiFormat = "F3")]
         public float heatingRate;
         
         public void Update()
@@ -18,7 +18,7 @@ namespace Thermal_Monitor
             if (!HighLogic.LoadedSceneIsFlight)
                 return;
 
-            heatingRate = (float)((part.thermalConductionFlux + part.thermalConvectionFlux + part.thermalRadiationFlux + part.thermalInternalFlux) / part.thermalMass);
+            heatingRate = (float)((part.thermalConductionFlux + part.thermalConvectionFlux + part.thermalRadiationFlux + part.thermalInternalFluxPrevious) / part.thermalMass);
             temp = (float)(part.temperature);
         }
     }
