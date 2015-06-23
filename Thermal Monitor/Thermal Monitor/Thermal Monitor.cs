92050954433e7f98061b6eb864f7ffd8bc6b61ce
Crzyrndm <joshcrawfy@gmail.com>
2015-06-23 14:40:53 +1200

    dont know how to calculate heating rates anymore

--- Thermal Monitor/Thermal Monitor/Thermal Monitor.cs
+++ Thermal Monitor/Thermal Monitor/Thermal Monitor.cs
@@ -8,18 +8,27 @@ namespace Thermal_Monitor
 {
     public class ThermalMonitor : PartModule
     {
-        [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Temperature", guiUnits = " K", guiFormat = "F2")]
-        public float temp;
-        [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Degrees / s", guiUnits = " K", guiFormat = "F3")]
-        public float heatingRate;
-        
-        public void Update()
+        [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Part")]
+        public string temp;
+        [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Part")]
+        public string heatingRate;
+
+        double lastSkinTemp, lastIntTemp;
+        double skinRate, internalRate;
+        public void FixedUpdate()
         {
             if (!HighLogic.LoadedSceneIsFlight)
                 return;
+            // a hack because how the heating rates should be calculated is not obvious
+            internalRate = internalRate * 0.9 + 0.1 * (part.temperature - lastIntTemp) / TimeWarp.fixedDeltaTime ;
+            lastIntTemp = part.temperature;
 
-            heatingRate = (float)((part.thermalConductionFlux + part.thermalConvectionFlux + part.thermalRadiationFlux + part.thermalInternalFluxPrevious) / part.thermalMass);
-            temp = (float)(part.temperature);
+            skinRate = skinRate * 0.99 + 0.01 * (part.skinTemperature - lastSkinTemp) / TimeWarp.fixedDeltaTime;
+            lastSkinTemp = part.skinTemperature;
+            //double internalRate = (part.thermalConductionFlux + part.thermalInternalFluxPrevious + part.skinInternalConductionMult * (part.skinTemperature - part.temperature)) / part.thermalMass;
+            //double skinRate = (part.thermalConvectionFlux + part.thermalRadiationFlux + part.skinInternalConductionMult * (part.temperature - part.skinTemperature)) / part.skinThermalMass;
+            temp = string.Format("{0:0.00}K | Skin: {1:0.00}K", part.temperature, part.skinTemperature);
+            heatingRate = string.Format("{0:0.00}K/s | Skin: {1:0.00}K/s", internalRate, skinRate);
         }
     }
 }
